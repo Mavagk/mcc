c483ffe7a4f37c28b29efce4771c1bfae96f610d
SwampMaster2160 <jaxon1454@gmail.com>
2026-01-14 12:58:54 +1300

    -

--- src/source_file_reader.rs
+++ src/source_file_reader.rs
@@ -1,9 +1,9 @@
-use std::{fs::File, io::{BufReader, Read}, num::NonZeroUsize, path::Path};
+use std::{fs::File, io::{self, BufReader, Bytes, Read}, num::NonZeroUsize, path::Path};
 
 use crate::error::Error;
 
 pub struct SourceFileReader<'a> {
-	reader: BufReader<File>,
+	reader: Bytes<BufReader<File>>,
 	path: &'a Path,
 	line: NonZeroUsize,
 	column: NonZeroUsize,
@@ -14,7 +14,7 @@ impl<'a> SourceFileReader<'a> {
 	pub fn new(path: &'a Path) -> Result<Self, Error> {
 		// Open file
 		let file = File::open(path).map_err(|err| Error::UnableToOpenFile(path.to_string_lossy().into(), err))?;
-		let reader = BufReader::new(file);
+		let reader = BufReader::new(file).bytes();
 		// Pack into struct
 		Ok(Self {
 			reader,
@@ -25,17 +25,46 @@ impl<'a> SourceFileReader<'a> {
 	}
 
 	pub fn peek_char(&mut self) -> Result<Option<char>, Error> {
-		// Read to buffer
-		let mut buffer: [u8; 4] = [0; 4];
-		let bytes_read = self.reader.read(&mut buffer).map_err(|err| Error::UnableToReadFile(self.path.to_string_lossy().into(), err))?;
-		// Rewind to before the read
-		self.reader.seek_relative(-(bytes_read as i64)).map_err(|err| Error::UnableToReadFile(self.path.to_string_lossy().into(), err))?;
-		// Return if we have reached the end of file or a null byte
-		if bytes_read == 0 || buffer[0] == 0 {
-			return Ok(None);
+		self.reader
+		//// Read to buffer
+		//let mut buffer: [u8; 4] = [0; 4];
+		//let bytes_read = self.reader.read(&mut buffer).map_err(|err| Error::UnableToReadFile(self.path.to_string_lossy().into(), err))?;
+		//// Rewind to before the read
+		//self.reader.seek_relative(-(bytes_read as i64)).map_err(|err| Error::UnableToReadFile(self.path.to_string_lossy().into(), err))?;
+		//// Return if we have reached the end of file or a null byte
+		//if bytes_read == 0 || buffer[0] == 0 {
+		//	return Ok(None);
+		//}
+		//// Convert bytes to char and return
+		//let chr = buffer.utf8_chunks().next().ok_or_else(|| Error::InvalidUtf8(self.path.to_string_lossy().into(), self.line, self.column))?.valid();
+		//Ok(Some(chr.chars().next().ok_or_else(|| Error::InvalidUtf8(self.path.to_string_lossy().into(), self.line, self.column))?))
+		todo!()
+	}
+}
+
+struct Utf8Iter {
+	bytes: Bytes<BufReader<File>>,
+}
+
+impl Iterator for Utf8Iter {
+	type Item = Result<char, io::Error>;
+
+	fn next(&mut self) -> Option<Result<char, io::Error>> {
+		let first_byte = match self.bytes.next()? {
+			Err(error) => return Some(Err(error)),
+			Ok(first_byte) => first_byte,
+		};
+		if first_byte < 0x80 {
+			return Some(Ok(first_byte as char));
 		}
-		// Convert bytes to char and return
-		let chr = buffer.utf8_chunks().next().ok_or_else(|| Error::InvalidUtf8(self.path.to_string_lossy().into(), self.line, self.column))?.valid();
-		Ok(Some(chr.chars().next().ok_or_else(|| Error::InvalidUtf8(self.path.to_string_lossy().into(), self.line, self.column))?))
+
+		let second_byte = match self.bytes.next()? {
+			Err(error) => return Some(Err(error)),
+			Ok(second_byte) => second_byte,
+		};
+		if second_byte < 0x80 {
+			return Some(Ok(first_byte as char));
+		}
+		todo!()
 	}
 }
