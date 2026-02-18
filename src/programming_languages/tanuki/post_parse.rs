f1508b144ce6d0c41ba01806f4f1ff81c01526a1
SwampMaster2160 <jaxon1454@gmail.com>
2026-02-18 16:44:30 +1300

    "

--- src/programming_languages/tanuki/post_parse.rs
+++ src/programming_languages/tanuki/post_parse.rs
@@ -1,7 +1,25 @@
-use crate::{Main, error::ErrorAt, programming_languages::tanuki::module::TanukiModule};
+use crate::{Main, error::ErrorAt, programming_languages::tanuki::{constant_value::TanukiConstantValue, expression::TanukiExpression, function::TanukiFunction, module::TanukiModule}};
+
+pub struct TanukiModulePostParseData<'a> {
+	pub functions: &'a mut Vec<TanukiFunction>,
+	pub global_constants: &'a mut Vec<TanukiConstantValue>,
+}
 
 impl TanukiModule {
-	pub fn post_parse(&mut self, _main: &mut Main) -> Result<(), ErrorAt> {
+	pub fn post_parse(&mut self, main: &mut Main) -> Result<(), ErrorAt> {
+		let post_parse_data = TanukiModulePostParseData {
+			functions: &mut self.functions,
+			global_constants: &mut self.global_constants,
+		};
+		for expression in self.parsed_expressions.iter_mut() {
+			expression.post_parse(main, &post_parse_data)?;
+		}
+		Ok(())
+	}
+}
+
+impl TanukiExpression {
+	pub fn post_parse(&mut self, main: &mut Main, post_parse_data: &TanukiModulePostParseData) -> Result<(), ErrorAt> {
 		Ok(())
 	}
 }
